0b7d9600e985677a34ae39882ca4f72bd37cf4e3
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-12 15:33:29 +0900

    Create test25.java

---
+++ test25.java
@@ -0,0 +1,25 @@
+public class test25 {
+    public static void main(String[] args) {
+        // 복합 연산자
+        // 서로 다른 연산자가 복합해서 쓰는 연산자
+        // 대입연산자 + 산술 또는 비트
+        
+        int bar = 10;
+
+        bar = bar+10;
+        bar +=10;
+       
+        bar = bar-10;
+        bar -=10;
+       
+        bar = bar*10;
+        bar  *= 10;
+       
+        bar = bar/10;
+        bar /=10;
+        
+        bar %=10;
+        bar = bar%10;
+    }
+    
+}
