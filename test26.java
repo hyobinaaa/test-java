804de115857f51b8cbb5e5f367f773dcc0aca674
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-12 15:54:38 +0900

    Create test26.java

---
+++ test26.java
@@ -0,0 +1,21 @@
+public class test26 {
+    public static void main(String[] args) {
+        // for 문 사용하기
+        // 1~100 사이 정수 중 짝수이면서,
+        for(int inputValue =2; inputValue<=100; inputValue+=2){
+             
+            // 8의 배수 또는 5의 배수인 값만 출력하라
+            if(inputValue%8 == 0 || inputValue%5==0){
+                System.out.println(inputValue+"");
+            }
+        }
+
+        
+        
+        
+
+
+        
+    }
+    
+}
