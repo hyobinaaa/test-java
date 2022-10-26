c80a0a9ca7cd47a9339712437133b4ec21fc6e8c
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-10-26 15:47:39 +0900

    Update test35.java

---
+++ test35.java
@@ -0,0 +1,65 @@
+public class test35 {
+    public static void main(String[] args) {
+//         - 흐름제어문에 종류
+// - 한국어로 전환이라는 의미
+// - switch → if else if or if else if else 흡사하다
+// - → 구간 비교가 불가
+// - → 두개 이상 선택 가능
+        // int n =4;
+
+        // switch(n){
+        //     case 1:
+        //         System.out.println(1);
+        //         break;
+        //     case 2:
+        //         System.out.println(2);
+        //         break;
+        //     case 3:
+        //         System.out.println(3);
+        //         break;
+
+        //     default:
+        //         System.out.println(4);
+        
+
+        // 내가 좋아 하는 랩퍼 입력 하기
+        
+        // 랩퍼들 나열 하기
+
+        // 변수값 설정
+
+        int inputValue = 1;
+        String rapper = "";
+
+        switch(inputValue){
+            case 1:
+            rapper = "창모";
+            break;
+
+            case 2:
+            rapper = "ph1";
+            break;
+
+            case 3:
+            rapper = "쌈디";
+            break;
+
+            case 4:
+            rapper = "스윙스";
+            break;
+            
+           
+        } 
+            System.out.println(rapper);
+        
+        
+      
+        }
+
+        }
+       
+            
+        
+     
+    
+
