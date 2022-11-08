7b173bd363be33de16a6bf2cf8365407d298144a
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-11-08 19:56:04 +0900

    Create test별찍기.java

---
+++ test별찍기.java
@@ -0,0 +1,20 @@
+public class test별찍기 {
+    public static void main(String[] args) {
+        // 별찍기 만들기
+
+        
+       // 별의 행 만들기
+       for(int stararow = 0 ; stararow<3 ; stararow++){
+        
+            // 별의 라인 만들기
+            for(int starline = 0 ; starline<4 ; starline++){
+
+                // 별 출력 하기
+                System.out.print("*");
+
+            }
+            System.out.println("");
+       }
+    }
+    
+}
