8cb0857f08ce44e0dfdaa8d7a7bfdee2de43332d
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-11-16 14:40:16 +0900

    Create test39.java

---
+++ test39.java
@@ -0,0 +1,14 @@
+public class test39 {
+    public static void main(String[] args) {
+        // continue 문 사용 해서 알파벳 출력
+
+        // 반드시 for문 사용해서 알파벳 연속 출력하기 
+
+        for(char j = 'z'; j >= 'a' ; j--){
+            System.out.println(j); // a를 거꾸로 만든다
+        }
+        
+
+    }
+    
+}
