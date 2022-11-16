1e4f5251a5da04cc1f6c31bc946c0d1629bebffc
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-11-16 14:40:09 +0900

    Create test38.java

---
+++ test38.java
@@ -0,0 +1,14 @@
+public class test38 {
+    public static void main(String[] args) {
+        // continue
+
+        for(int i = 0 ; i < 10 ; i++) {
+            if(i % 2 == 0)
+                continue;
+
+            System.out.println(i);
+            }
+        }
+    }
+    
+
