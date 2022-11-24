0db5f887a08f5e73cb333b997720da4fc4d4bafe
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-11-24 19:39:01 +0900

    Create test42.java

---
+++ test42.java
@@ -0,0 +1,13 @@
+public class test42 {
+    public static void main(String[] args) {
+        
+        // h e l l o
+       
+
+       char bar[] = new char[]{'h' , 'e' , 'l' , 'l', 'o'}; // 배열의 원소는 5개
+
+       for(int i = 0 ; i < bar.length ; i++)
+           System.out.println(bar[i]);
+    }
+    
+}
