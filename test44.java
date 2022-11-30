b27c32b548f59ab6eeebd9f426f9184b4d86d270
leehyobin2 <109259552+leehyobin2@users.noreply.github.com>
2022-11-30 16:59:02 +0900

    Update test44.java

---
+++ test44.java
@@ -0,0 +1,16 @@
+public class test44 {
+    public static void main(String[] args) {
+        // 2차원
+        
+        // 2 x 3
+        int bar[][] = new int [] [] {{1, 2 , 3 }, {4, 5, 6}};
+
+
+        int foo[];
+        
+        foo = bar[1];
+
+          System.out.println(foo[2]);
+    }
+    
+}
